8866dc9186e1f725700b591c7a731db1541439b6
Alexander Mihailov <53320300+alexmegadrive@users.noreply.github.com>
2022-08-31 06:40:57 +0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -7,20 +7,18 @@ var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0]; 
 btn.onclick = function() {
     modal.style.display = "block";
-    //modal.classList.add('active'); 
-//
+    
+
 }
 
 span.onclick = function() {
     modal.style.display = "none";
 }
-// span.onclick = function() {
-//     modal.style.display = "none";
-// }
+
 window.onclick = function(event) {
     if (event.target == modal) {
          modal.style.display = "none";
-      // modal.classList.remove('active'); 
+       
 
     }
 }
